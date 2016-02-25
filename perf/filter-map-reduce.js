8b1f2d3e8bea9e4662f1160aeeb891463ec02ee3
Andre Medeiros <andre.medeiros@futurice.com>
2016-02-26 00:16:51 +0200

    perf(filter-map-reduce): add preliminary perf micro benchmarks

---
+++ perf/filter-map-reduce.js
@@ -0,0 +1,73 @@
+var Benchmark = require('benchmark');
+var xstream = require('../lib/index').default;
+var most = require('most');
+var rx = require('rx');
+var rxjs = require('@reactivex/rxjs')
+var kefir = require('kefir');
+var bacon = require('baconjs');
+var lodash = require('lodash');
+var highland = require('highland');
+
+var runners = require('./runners');
+var kefirFromArray = runners.kefirFromArray;
+
+// Create a stream from an Array of n integers
+// filter out odds, map remaining evens by adding 1, then reduce by summing
+var n = runners.getIntArg(1000000);
+var a = new Array(n);
+for(var i = 0; i< a.length; ++i) {
+  a[i] = i;
+}
+
+var suite = Benchmark.Suite('filter -> map -> reduce ' + n + ' integers');
+var options = {
+  defer: true,
+  onError: function(e) {
+    e.currentTarget.failure = e.error;
+  }
+};
+
+suite
+  .add('most', function(deferred) {
+    runners.runMost(deferred, most.from(a).filter(even).map(add1).reduce(sum, 0));
+  }, options)
+  .add('rx 4', function(deferred) {
+    runners.runRx(deferred, rx.Observable.fromArray(a).filter(even).map(add1))//.reduce(sum, 0));
+  }, options)
+  .add('rx 5', function(deferred) {
+    runners.runRx5(deferred,
+      rxjs.Observable.fromArray(a).filter(even).map(add1))//.reduce(sum, 0));
+  }, options)
+  .add('xstream', function(deferred) {
+    runners.runXStream(deferred,
+      xstream.from(a).filter(even).map(add1))//.reduce(sum, 0));
+  }, options)
+  .add('kefir', function(deferred) {
+    runners.runKefir(deferred, kefirFromArray(a).filter(even).map(add1).scan(sum, 0).last());
+  }, options)
+  .add('bacon', function(deferred) {
+    runners.runBacon(deferred, bacon.fromArray(a).filter(even).map(add1))//.reduce(0, sum));
+  }, options)
+  .add('highland', function(deferred) {
+    runners.runHighland(deferred, highland(a).filter(even).map(add1))//.reduce(0, sum));
+  }, options)
+  .add('lodash', function() {
+    return lodash(a).filter(even).map(add1)//.reduce(sum, 0);
+  })
+  .add('Array', function() {
+    return a.filter(even).map(add1)//.reduce(sum, 0);
+  });
+
+runners.runSuite(suite);
+
+function add1(x) {
+  return x + 1;
+}
+
+function even(x) {
+  return x % 2 === 0;
+}
+
+function sum(x, y) {
+  return x + y;
+}
---
+++ perf/runners.js
@@ -0,0 +1,174 @@
+var kefir = require('kefir');
+kefir.DEPRECATION_WARNINGS = false;
+
+exports.runSuite       = runSuite;
+
+exports.runMost        = runMost;
+exports.runRx          = runRx;
+exports.runRx5         = runRx5;
+exports.runXStream     = runXStream;
+exports.runKefir       = runKefir;
+exports.kefirFromArray = kefirFromArray;
+exports.runBacon       = runBacon;
+exports.runHighland    = runHighland;
+
+exports.getIntArg      = getIntArg;
+exports.getIntArg2     = getIntArg2;
+exports.logResults     = logResults;
+
+function noop() {}
+
+function _getIntArg(defaultValue, index) {
+  var n = parseInt(process.argv[index]);
+  return isNaN(n) ? defaultValue : n;
+}
+
+function getIntArg(defaultValue) {
+  return _getIntArg(defaultValue, process.argv.length - 1);
+}
+
+function getIntArg2(default1, default2) {
+  var m = _getIntArg(default1, process.argv.length - 2);
+  var n = _getIntArg(default2, process.argv.length - 1);
+  return [m, n];
+}
+
+function logResults(e) {
+  var t = e.target;
+
+  if(t.failure) {
+    console.error(padl(10, t.name) + 'FAILED: ' + e.target.failure);
+  } else {
+    var result = padl(10, t.name)
+      + padr(13, t.hz.toFixed(2) + ' op/s')
+      + ' \xb1' + padr(7, t.stats.rme.toFixed(2) + '%')
+      + padr(15, ' (' + t.stats.sample.length + ' samples)');
+
+    console.log(result);
+  }
+}
+
+function logStart() {
+  console.log(this.name);
+  console.log('-------------------------------------------------------');
+}
+
+function logComplete() {
+  console.log('-------------------------------------------------------');
+}
+
+function runSuite(suite) {
+  return suite
+    .on('start', logStart)
+    .on('cycle', logResults)
+    .on('complete', logComplete)
+    .run();
+}
+
+function runMost(deferred, mostPromise) {
+  mostPromise.then(function() {
+    deferred.resolve();
+  }, function(e) {
+    deferred.benchmark.emit({ type: 'error', error: e });
+    deferred.resolve(e);
+  });
+}
+
+function runRx(deferred, rxStream) {
+  rxStream.subscribe({
+    onNext: noop,
+    onCompleted: function() {
+      deferred.resolve();
+    },
+    onError: function(e) {
+      deferred.benchmark.emit({ type: 'error', error: e });
+      deferred.resolve(e);
+    }
+  });
+}
+
+function runRx5(deferred, rxStream) {
+  rxStream.subscribe({
+    next: noop,
+    complete: function() {
+      deferred.resolve();
+    },
+    error: function(e) {
+      deferred.benchmark.emit({ type: 'error', error: e });
+      deferred.resolve(e);
+    }
+  });
+}
+
+function runXStream(deferred, xstream) {
+  xstream.subscribe({
+    next: noop,
+    complete: function() {
+      deferred.resolve();
+    },
+    error: function(e) {
+      deferred.benchmark.emit({ type: 'error', error: e });
+      deferred.resolve(e);
+    }
+  });
+}
+
+function runKefir(deferred, kefirStream) {
+  kefirStream.onValue(noop);
+  kefirStream.onEnd(function() {
+    deferred.resolve();
+  });
+}
+
+function kefirFromArray(array) {
+  return kefir.stream(function(emitter) {
+    for(var i=0; i<array.length; ++i) {
+      emitter.emit(array[i]);
+    }
+    emitter.end();
+  });
+}
+
+function runBacon(deferred, baconStream) {
+  try {
+    baconStream.onValue(noop);
+    baconStream.onEnd(function() {
+      deferred.resolve();
+    });
+    baconStream.onError(function(e) {
+      deferred.benchmark.emit({ type: 'error', error: e });
+      deferred.resolve(e);
+    });
+  } catch(e) {
+    deferred.benchmark.emit({ type: 'error', error: e });
+    deferred.resolve(e);
+  }
+}
+
+// Using pull() seems to give the fastest results for highland,
+// but will only work for test runs that reduce a stream to a
+// single value.
+function runHighland(deferred, highlandStream) {
+  highlandStream.pull(function(err, z) {
+    if(err) {
+      deferred.reject(err);
+      return;
+    }
+
+    deferred.resolve(z);
+  });
+}
+
+function padl(n, s) {
+  while(s.length < n) {
+    s += ' ';
+  }
+  return s;
+}
+
+function padr(n, s) {
+  while (s.length < n) {
+    s = ' ' + s;
+  }
+  return s;
+}
