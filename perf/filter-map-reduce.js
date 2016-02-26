7ff9fd097c743472a711640b39811971bb1ee773
Andre Medeiros <andre.medeiros@futurice.com>
2016-02-26 11:30:30 +0200

    perf(filter-map-reduce): improve filter-map-reduce test to actually do reduce() too

--- perf/filter-map-reduce.js
+++ perf/filter-map-reduce.js
@@ -32,30 +32,30 @@ suite
     runners.runMost(deferred, most.from(a).filter(even).map(add1).reduce(sum, 0));
   }, options)
   .add('rx 4', function(deferred) {
-    runners.runRx(deferred, rx.Observable.fromArray(a).filter(even).map(add1))//.reduce(sum, 0));
+    runners.runRx(deferred, rx.Observable.fromArray(a).filter(even).map(add1).reduce(sum, 0));
   }, options)
   .add('rx 5', function(deferred) {
     runners.runRx5(deferred,
-      rxjs.Observable.fromArray(a).filter(even).map(add1))//.reduce(sum, 0));
+      rxjs.Observable.fromArray(a).filter(even).map(add1).reduce(sum, 0));
   }, options)
   .add('xstream', function(deferred) {
     runners.runXStream(deferred,
-      xstream.from(a).filter(even).map(add1))//.reduce(sum, 0));
+      xstream.from(a).filter(even).map(add1).fold(sum, 0).last());
   }, options)
   .add('kefir', function(deferred) {
     runners.runKefir(deferred, kefirFromArray(a).filter(even).map(add1).scan(sum, 0).last());
   }, options)
   .add('bacon', function(deferred) {
-    runners.runBacon(deferred, bacon.fromArray(a).filter(even).map(add1))//.reduce(0, sum));
+    runners.runBacon(deferred, bacon.fromArray(a).filter(even).map(add1).reduce(0, sum));
   }, options)
   .add('highland', function(deferred) {
-    runners.runHighland(deferred, highland(a).filter(even).map(add1))//.reduce(0, sum));
+    runners.runHighland(deferred, highland(a).filter(even).map(add1).reduce(0, sum));
   }, options)
   .add('lodash', function() {
-    return lodash(a).filter(even).map(add1)//.reduce(sum, 0);
+    return lodash(a).filter(even).map(add1).reduce(sum, 0);
   })
   .add('Array', function() {
-    return a.filter(even).map(add1)//.reduce(sum, 0);
+    return a.filter(even).map(add1).reduce(sum, 0);
   });
 
 runners.runSuite(suite);
