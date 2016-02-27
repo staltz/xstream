9f766af6c25daa70c54291422c95a047a915ead1
Andre Medeiros <andre.medeiros@futurice.com>
2016-02-27 14:22:29 +0200

    perf(debug): improve performance of debug() operator, using Proxy class

--- src/operator/DebugProducer.ts
+++ src/operator/DebugProducer.ts
@@ -3,6 +3,29 @@ import {Producer} from '../Producer';
 import {Stream} from '../Stream';
 import {emptyObserver} from '../utils/emptyObserver';
 
+export class Proxy<T> implements Observer<T> {
+  constructor(public out: Stream<T>,
+              public p: DebugProducer<T>) {
+  }
+
+  next(t: T) {
+    if (this.p.spy) {
+      this.p.spy(t);
+    } else {
+      console.log(t);
+    }
+    this.out.next(t);
+  }
+
+  error(err: any) {
+    this.out.error(err);
+  }
+
+  end() {
+    this.out.end();
+  }
+}
+
 export class DebugProducer<T> implements Producer<T> {
   public proxy: Observer<T> = emptyObserver;
 
@@ -11,19 +34,7 @@ export class DebugProducer<T> implements Producer<T> {
   }
 
   start(out: Stream<T>): void {
-    this.proxy = {
-      next: (t: T) => {
-        if (this.spy) {
-          this.spy(t);
-        } else {
-          console.log(t);
-        }
-        out.next(t);
-      },
-      error: (err) => out.error(err),
-      end: () => out.end(),
-    };
-    this.ins.subscribe(this.proxy);
+    this.ins.subscribe(this.proxy = new Proxy(out, this));
   }
 
   stop(): void {
