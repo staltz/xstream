5233f4323e090b4a8a08e97c9ac818ded47ff20d
Andre Medeiros <andre.medeiros@futurice.com>
2016-02-27 14:28:07 +0200

    perf(skip): improve skip perf by using Proxy Observer class

--- src/operator/SkipProducer.ts
+++ src/operator/SkipProducer.ts
@@ -3,6 +3,24 @@ import {Producer} from '../Producer';
 import {Stream} from '../Stream';
 import {emptyObserver} from '../utils/emptyObserver';
 
+export class Proxy<T> implements Observer<T> {
+  constructor(public out: Stream<T>,
+              public prod: SkipProducer<T>) {
+  }
+
+  next(t: T) {
+    if (this.prod.skipped++ >= this.prod.max) this.out.next(t);
+  }
+
+  error(err: any) {
+    this.out.error(err);
+  }
+
+  end() {
+    this.out.end();
+  }
+}
+
 export class SkipProducer<T> implements Producer<T> {
   public proxy: Observer<T> = emptyObserver;
   public skipped: number = 0;
@@ -12,14 +30,7 @@ export class SkipProducer<T> implements Producer<T> {
   }
 
   start(out: Stream<T>): void {
-    this.proxy = {
-      next: (t: T) => {
-        if (this.skipped++ >= this.max) out.next(t);
-      },
-      error: (err) => out.error(err),
-      end: () => out.end(),
-    };
-    this.ins.subscribe(this.proxy);
+    this.ins.subscribe(this.proxy = new Proxy(out, this));
   }
 
   stop(): void {
