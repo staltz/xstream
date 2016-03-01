a9c5c6e7c62a45aa9269e7086ff4400188878fbf
Andre Medeiros <andre.medeiros@futurice.com>
2016-03-01 21:28:33 +0200

    test(remember): add a test for remember() operator

--- tests/operator/remember.ts
+++ tests/operator/remember.ts
@@ -3,6 +3,26 @@ import {emptyObserver} from '../../src/utils/emptyObserver';
 import * as assert from 'assert';
 
 describe('Stream.prototype.remember', () => {
+  it('should replay the second event to a new observer', (done) => {
+    const stream = xs.interval(50).take(4).remember();
+
+    stream.subscribe(emptyObserver);
+
+    let expected = [1, 2, 3];
+    setTimeout(() => {
+      stream.subscribe({
+        next(x) {
+          assert.strictEqual(x, expected.shift());
+        },
+        error: done.fail,
+        end: () => {
+          assert.strictEqual(expected.length, 0);
+          done();
+        }
+      });
+    }, 125);
+  });
+
   it('should replay the last event to a new observer', (done) => {
     const stream = xs.interval(1).take(6).remember();
 
