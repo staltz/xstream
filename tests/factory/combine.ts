afa808bf4c1ef1678cab4868e3e82ec5501aa3c8
Andre Medeiros <andre.medeiros@futurice.com>
2016-03-06 20:59:52 +0200

    test(combine): add end-semantics tests for combine()

--- tests/factory/combine.ts
+++ tests/factory/combine.ts
@@ -2,11 +2,28 @@ import xs from '../../src/index';
 import * as assert from 'assert';
 
 describe('xs.combine', () => {
-  it('should merge AND-style two streams together', (done) => {
+  it('should combine AND-style two streams together', (done) => {
     const stream1 = xs.interval(100).take(2);
     const stream2 = xs.interval(120).take(2);
     const stream = xs.combine((x, y) => `${x}${y}`, stream1, stream2);
     let expected = ['00', '10', '11'];
+    stream.subscribe({
+      next: (x) => {
+        assert.equal(x, expected.shift());
+      },
+      error: done.fail,
+      end: () => {
+        assert.equal(expected.length, 0);
+        done();
+      },
+    });
+  });
+
+  it('should end only when all member streams have ended', (done) => {
+    const stream1 = xs.interval(30).take(1);
+    const stream2 = xs.interval(50).take(4);
+    const stream = xs.combine((x, y) => `${x}${y}`, stream1, stream2);
+    let expected = ['00', '01', '02', '03'];
     stream.subscribe({
       next: (x) => {
         assert.equal(x, expected.shift());
--- tests/operator/combine.ts
+++ tests/operator/combine.ts
@@ -18,4 +18,21 @@ describe('Stream.prototype.combine', () => {
       },
     });
   });
+
+  it('should end only when all member streams have ended', (done) => {
+    const source = xs.interval(30).take(1);
+    const other = xs.interval(50).take(4);
+    const stream = source.combine((x, y) => `${x}${y}`, other);
+    let expected = ['00', '01', '02', '03'];
+    stream.subscribe({
+      next: (x) => {
+        assert.equal(x, expected.shift());
+      },
+      error: done.fail,
+      end: () => {
+        assert.equal(expected.length, 0);
+        done();
+      },
+    });
+  });
 });
