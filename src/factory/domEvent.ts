628a6ba11c40163dfad8dd40172a2409287cd51c
Andre Medeiros <andre.medeiros@futurice.com>
2016-03-06 18:59:09 +0200

    refactor(domEvent): remove unnecessary this.out from EventProducer

--- src/factory/domEvent.ts
+++ src/factory/domEvent.ts
@@ -3,16 +3,14 @@ import {Observer} from '../Observer';
 import {Stream} from '../Stream';
 
 class EventProducer implements Producer<Event> {
-  private out: Observer<Event>;
   private listener: EventListener;
 
-  constructor(public node: EventTarget, public eventType: string,
+  constructor(public node: EventTarget,
+              public eventType: string,
               public useCapture: boolean) {
   }
 
-  // use of any is to avoid Event type conflict
   start(out: Observer<Event>) {
-    this.out = out;
     this.listener = (e) => out.next(e);
     const {node, eventType, useCapture} = this;
     node.addEventListener(eventType, this.listener, useCapture);
