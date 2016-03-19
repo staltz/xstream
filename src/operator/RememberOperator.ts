08984043c2b17d5ae91dd3ce8b066af8fca68b2b
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-03-19 07:39:47 -0400

    feat(RememberOperator): adjust to work with MemoryStream
    RememberOperator simply flows values through from a Stream -> MemoryStream

--- src/operator/RememberOperator.ts
+++ src/operator/RememberOperator.ts
@@ -1,36 +1,14 @@
-import {Observer} from '../Observer';
 import {Operator} from '../Operator';
-import {Stream} from '../Stream';
-import {emptyObserver} from '../utils/emptyObserver';
-
-export class Proxy<T> implements Observer<T> {
-  constructor(public out: Stream<T>,
-              public p: RememberOperator<T>) {
-  }
-
-  next(t: T) {
-    this.out.next(t);
-    this.out._val = t;
-  }
-
-  error(err: any) {
-    this.out.error(err);
-  }
-
-  end() {
-    this.out.end();
-  }
-}
+import {Stream, MemoryStream} from '../Stream';
 
 export class RememberOperator<T> implements Operator<T, T> {
-  public proxy: Observer<T> = emptyObserver;
   public value: any;
 
   constructor(public ins: Stream<T>) {
   }
 
-  start(out: Stream<T>): void {
-    this.ins.subscribe(this.proxy = new Proxy(out, this));
+  start(out: MemoryStream<T>): void {
+    this.ins.subscribe(out);
   }
 
   stop(): void {
