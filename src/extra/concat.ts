765201195aab222ee424c8948da3785c84f1dc74
Andre Medeiros <andre.medeiros@futurice.com>
2016-04-10 19:42:45 +0300

    feat(concat): implement extra concat() operator

---
+++ src/extra/concat.ts
@@ -0,0 +1,46 @@
+import {InternalProducer} from '../InternalProducer';
+import {InternalListener} from '../InternalListener';
+import {Stream} from '../Stream';
+
+class ConcatProducer<T> implements InternalProducer<T>, InternalListener<T> {
+  private out: InternalListener<T> = null;
+  private i: number = 0;
+
+  constructor(public streams: Array<Stream<T>>) {
+  }
+
+  _start(out: InternalListener<T>): void {
+    this.out = out;
+    this.streams[this.i]._add(this);
+  }
+
+  _stop(): void {
+    const streams = this.streams;
+    if (this.i < streams.length) {
+      streams[this.i]._remove(this);
+    }
+    this.i = 0;
+  }
+
+  _n(t: T) {
+    this.out._n(t);
+  }
+
+  _e(err: any) {
+    this.out._e(err);
+  }
+
+  _c() {
+    const streams = this.streams;
+    streams[this.i]._remove(this);
+    if (++this.i < streams.length) {
+      streams[this.i]._add(this);
+    } else {
+      this.out._c();
+    }
+  }
+}
+
+export default function concat<T>(...streams: Array<Stream<T>>): Stream<T> {
+  return new Stream<T>(new ConcatProducer(streams));
+}
---
+++ tests/extra/concat.ts
@@ -0,0 +1,60 @@
+import xs from '../../src/index';
+import concat from '../../src/extra/concat';
+import * as assert from 'assert';
+
+describe('concat (extra)', () => {
+  it('should concatenate two synchronous short streams together', (done) => {
+    const stream1 = xs.of(1, 2, 3);
+    const stream2 = xs.of(40, 50, 60, 70);
+    const stream3 = xs.of(8, 9);
+    const stream = concat(stream1, stream2, stream3);
+    const expected = [1, 2, 3, 40, 50, 60, 70, 8, 9];
+
+    stream.addListener({
+      next: (x: number) => {
+        assert.equal(x, expected.shift());
+      },
+      error: (err: Error) => done(err),
+      complete: () => {
+        assert.equal(expected.length, 0);
+        done();
+      },
+    });
+  });
+
+  it('should concatenate two asynchronous short streams together', (done) => {
+    const stream1 = xs.periodic(50).take(3);
+    const stream2 = xs.periodic(100).take(2);
+    const stream = concat(stream1, stream2);
+    const expected = [0, 1, 2, 0, 1];
+
+    stream.addListener({
+      next: (x: number) => {
+        assert.equal(x, expected.shift());
+      },
+      error: (err: Error) => done(err),
+      complete: () => {
+        assert.equal(expected.length, 0);
+        done();
+      },
+    });
+  });
+
+  it('should append a synchronous stream after an asynchronous stream', (done) => {
+    const stream1 = xs.periodic(50).take(3);
+    const stream2 = xs.of(30, 40, 50, 60);
+    const stream = concat(stream1, stream2);
+    const expected = [0, 1, 2, 30, 40, 50, 60];
+
+    stream.addListener({
+      next: (x: number) => {
+        assert.equal(x, expected.shift());
+      },
+      error: (err: Error) => done(err),
+      complete: () => {
+        assert.equal(expected.length, 0);
+        done();
+      },
+    });
+  });
+});
