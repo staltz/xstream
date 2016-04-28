85e403357bbf7cdcb01f8b0293f72ea76c1a1f5e
Andre Staltz <andre@staltz.com>
2016-04-28 22:41:36 +0300

    fix(Stream): WIP! an attempt to fix ils mutation concurrency

--- src/extra/concat.ts
+++ src/extra/concat.ts
@@ -1,24 +1,8 @@
 import {Stream, InternalProducer, InternalListener} from '../core';
 
-class ConcatProducer<T> implements InternalProducer<T>, InternalListener<T> {
-  private out: InternalListener<T> = null;
-  private i: number = 0;
-
-  constructor(public streams: Array<Stream<T>>) {
-  }
-
-  _start(out: InternalListener<T>): void {
-    this.out = out;
-    this.streams[this.i]._add(this);
-  }
-
-  _stop(): void {
-    const streams = this.streams;
-    if (this.i < streams.length) {
-      streams[this.i]._remove(this);
-    }
-    this.i = 0;
-    this.out = null;
+class ConcatListener<T> implements InternalListener<T> {
+  constructor(private out: InternalListener<T>,
+              private p: ConcatProducer<T>) {
   }
 
   _n(t: T) {
@@ -30,12 +14,41 @@ class ConcatProducer<T> implements InternalProducer<T>, InternalListener<T> {
   }
 
   _c() {
+    this.p.less();
+  }
+}
+
+class ConcatProducer<T> implements InternalProducer<T> {
+  private out: InternalListener<T> = null;
+  private li: ConcatListener<T> = null;
+  private i: number = 0;
+
+  constructor(public streams: Array<Stream<T>>) {
+  }
+
+  _start(out: InternalListener<T>): void {
+    this.out = out;
+    this.streams[this.i]._add(this.li = new ConcatListener(out, this));
+  }
+
+  _stop(): void {
     const streams = this.streams;
-    streams[this.i]._remove(this);
+    if (this.i < streams.length) {
+      streams[this.i]._remove(this.li);
+    }
+    this.out = null;
+    this.li = null;
+    this.i = 0;
+  }
+
+  less() {
+    const streams = this.streams;
+    const out = this.out;
+    streams[this.i]._remove(this.li);
     if (++this.i < streams.length) {
-      streams[this.i]._add(this);
+      streams[this.i]._add(this.li = new ConcatListener(out, this));
     } else {
-      this.out._c();
+      out._c();
     }
   }
 }
