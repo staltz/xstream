2909a78c53f8be99826c021053f4b6c7c0b062e9
Andre Staltz <andre@staltz.com>
2016-09-20 20:34:26 +0300

    fix(merge): increase variadic type count to max 10

--- src/core.ts
+++ src/core.ts
@@ -211,6 +211,44 @@ export interface MergeSignature {
     s4: Stream<T4>,
     s5: Stream<T5>,
     s6: Stream<T6>): Stream<T1 | T2 | T3 | T4 | T5 | T6>;
+  <T1, T2, T3, T4, T5, T6, T7>(
+    s1: Stream<T1>,
+    s2: Stream<T2>,
+    s3: Stream<T3>,
+    s4: Stream<T4>,
+    s5: Stream<T5>,
+    s6: Stream<T6>,
+    s7: Stream<T7>): Stream<T1 | T2 | T3 | T4 | T5 | T6 | T7>;
+  <T1, T2, T3, T4, T5, T6, T7, T8>(
+    s1: Stream<T1>,
+    s2: Stream<T2>,
+    s3: Stream<T3>,
+    s4: Stream<T4>,
+    s5: Stream<T5>,
+    s6: Stream<T6>,
+    s7: Stream<T7>,
+    s8: Stream<T7>): Stream<T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8>;
+  <T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+    s1: Stream<T1>,
+    s2: Stream<T2>,
+    s3: Stream<T3>,
+    s4: Stream<T4>,
+    s5: Stream<T5>,
+    s6: Stream<T6>,
+    s7: Stream<T7>,
+    s8: Stream<T7>,
+    s9: Stream<T7>): Stream<T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9>;
+  <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
+    s1: Stream<T1>,
+    s2: Stream<T2>,
+    s3: Stream<T3>,
+    s4: Stream<T4>,
+    s5: Stream<T5>,
+    s6: Stream<T6>,
+    s7: Stream<T7>,
+    s8: Stream<T7>,
+    s9: Stream<T7>,
+    s10: Stream<T7>): Stream<T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9 | T10>;
   <T>(...stream: Array<Stream<T>>): Stream<T>;
 }
 
