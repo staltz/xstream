708cbf47f86b49dd067d233dac4422779a3aa76a
Volodymyr Kobeliatskyi <vladimir.kobeliatsky@gmail.com>
2017-05-11 11:45:02 +0300

    feat(extra): buffer operator that splits input stream into arrays by separator stream
    buffer operator accepts a separator stream as an argument. It accumulates input stream events into
    arrays and emits them when separator stream emits or closes. Output stream is closed when separatos
    stream is closed. #goodnessSquad
    
    none

---
+++ src/extra/buffer.ts
@@ -0,0 +1,122 @@
+import {Operator, Stream, Subscription, InternalListener, OutSender, NO_IL} from '../index';
+
+class SeparatorIL<T> implements InternalListener<any>, OutSender<Array<T>> {
+    constructor(public out: Stream<Array<T>>,
+                private op: BufferOperator<T>) {
+    }
+
+    _n(t: any) {
+        this.op.flush();
+    }
+
+    _e(err: any) {
+        this.out._e(err);
+    }
+
+    _c() {
+        this.op.flush();
+        this.out._c();
+    }
+}
+
+class BufferOperator<T> implements Operator<T, Array<T>> {
+
+    public type = 'buffer';
+    public out: Stream<Array<T>> = null as any;
+
+    private sil: InternalListener<any>;
+    private acc: Array<T> = [];
+
+    constructor(
+        public s: Stream<any>,
+        public ins: Stream<T>
+        ) {}
+
+    flush() {
+        if (this.acc.length > 0) {
+            this.out._n(this.acc);
+            this.acc = [];
+        }
+    }
+
+    _start(out: Stream<Array<T>>): void {
+        this.out = out;
+        this.ins._add(this);
+        this.sil = new SeparatorIL(out, this);
+        this.s._add(this.sil);
+    }
+
+    _stop(): void {
+        this.flush();
+        this.ins._remove(this);
+        this.out = null as any;
+        this.s._remove(this.sil);
+        this.sil = NO_IL;
+    }
+
+    _n(t: T) {
+        this.acc.push(t);
+    }
+
+    _e(err: any) {
+        const u = this.out;
+        if (!u) return;
+        u._e(err);
+    }
+
+    _c() {
+        const out = this.out;
+        if (!out) return;
+    }
+}
+
+/**
+ * Buffers a stream using a separator stream. Returns a stream that emits
+ * arrays.
+ *
+ * Marble diagram:
+ *
+ * ```text
+ * --1--2--3--4--5--6--7--8--9|
+ *  split( -a---------b---------c| )
+ *          :         :         :
+ * ---------[ 1       :         :
+ *          , 2       :         :
+ *          , 3       :         :
+ *          ]---------[ 4       :
+ *                    , 5       :
+ *                    , 6       :
+ *                    ]---------[ 7
+ *                              , 8
+ *                              , 9
+ *                              ]|
+ * ```
+ *
+ * Example:
+ *
+ * ```js
+ * import buffer from 'xstream/extra/buffer'
+ *
+ * const source = xs.periodic(50).take(10);
+ * const separator = xs.periodic(170).take(3);
+ * const buffered = source.compose(buffer(separator));
+ *
+ * buffered.addListener({
+ *   next: arr => console.log(arr),
+ *   error: err => console.error(err)
+ * });
+ * ```
+ *
+ * ```text
+ * > [0, 1, 2]
+ * > [3, 4, 5]
+ * > [6, 7, 8]
+ * ```
+ *
+ * @param {Stream} separator Some other stream that is used to know when to
+ * split the output stream.
+ * @return {Stream}
+ */
+export default function buffer<T>(s: Stream<any>): <T>(ins: Stream<T>) => Stream<Array<T>> {
+    return (ins: Stream<T>) => new Stream<Array<T>>(new BufferOperator<T>(s, ins));
+}
